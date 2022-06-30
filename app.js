e6d56f383ad52e5aeaa1c6ca5b5a7f1aadfa39ff
carlosrodriguez2020 <carlosmvrodriguez9@gmail.com>
2022-06-30 14:01:56 -0300

    method post created and test from postman, via /usuario path

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.get("/", (req, res) => {
   res.send("hello mi ray");
+});
+
+app.post("/usuario", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
 });
 
 app.listen(port, () => {
